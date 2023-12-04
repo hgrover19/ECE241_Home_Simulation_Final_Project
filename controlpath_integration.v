ae08775e0c25382cb39bf0144affca5909639951
joon2022park <90308427+joon2022park@users.noreply.github.com>
2023-12-04 03:29:55 -0500

    Update controlpath_integration.v

--- controlpath_integration.v
+++ controlpath_integration.v
@@ -1,4 +1,3 @@
-
 module controlpath(
 
 	input loadinputs, //the pushbutton, loadinputs
@@ -8,16 +7,21 @@ module controlpath(
 	input keyboardin, //keyboard input (L or D)
 	input audin,	//Audio input (on or off, 1/0)
 	input room0, room1, room2, room3, room4, //switch input (select room number 0-9)
-	input alllocked, //switch input (for "all locked")
 	input countDone, //VGA Datapath sends countDone = 1 when finished drawing
-	
+	input [3:0] plotcounter,
+	input [7:0] MAX_X_PIXELS,
+	input [6:0] MAX_Y_PIXELS,
+	input [7:0] clear_x,
+	input [6:0] clear_y,
+
 	output reg enable0, enable1, enable2, enable3, enable4, //signals to select which room we load function and ON/OFF to
 	output reg selonoff, //sort stored audio registers by ON/OFF for audio datapath
 	output reg [1:0] selfunct, //sort stored audio registers by Light/Door function
 	output reg clearinitsignal,
 	output reg loadenable, //enable loading of inputs
 	output reg [2:0] selsw,
-	output reg commandaudioenable
+	output reg commandaudioenable,
+	output reg drawen
 	
 );
 
@@ -32,12 +36,11 @@ module controlpath(
 		ROOM2 = 6'd4,
 		ROOM3 = 6'd5,
 		ROOM4 = 6'd6,
-		ALLLOCKED = 6'd7,
 		
-		DONE_DRAW = 6'd8, //Finish drawing VGA display image, so set Done = 1
-		DONE = 6'd9,	//Reach this state when Done = 1
-		CLEAR = 6'd10, //Clear VGA Display of everything
-		DONE_CLEAR = 6'd11; //When finished clearing, set Done = 1
+		DONE_DRAW = 6'd7, //Finish drawing VGA display image, so set Done = 1
+		DONE = 6'd8,	//Reach this state when Done = 1
+		CLEAR = 6'd9, //Clear VGA Display of everything
+		DONE_CLEAR = 6'd10; //When finished clearing, set Done = 1
 		
 	
 	//State table:
@@ -64,27 +67,24 @@ module controlpath(
 							next_state = ROOM3;
 						else if (room4 == 1'b1)
 							next_state = ROOM4;
-						else if (alllocked == 1'b1)
-							next_state = ALLLOCKED;
+							
 				end
 				
-				ROOM0: next_state = countDone ? DONE_DRAW : ROOM0;
+				ROOM0: next_state = (plotcounter == 4'd15) ? DONE_DRAW : ROOM0;
 				
-				ROOM1: next_state = countDone ? DONE_DRAW : ROOM1;
+				ROOM1: next_state = (plotcounter == 4'd15) ? DONE_DRAW : ROOM1;
 				
-				ROOM2: next_state = countDone ? DONE_DRAW : ROOM2;
+				ROOM2: next_state = (plotcounter == 4'd15) ? DONE_DRAW : ROOM2;
 				
-				ROOM3: next_state = countDone ? DONE_DRAW : ROOM3;
+				ROOM3: next_state = (plotcounter == 4'd15) ? DONE_DRAW : ROOM3;
 				
-				ROOM4: next_state = countDone ? DONE_DRAW : ROOM4;
-				
-				ALLLOCKED: next_state = DONE;
+				ROOM4: next_state = (plotcounter == 4'd15) ? DONE_DRAW : ROOM4;
 				
 				DONE_DRAW: next_state = DONE;
 				
 				DONE: next_state = LOAD_INPUTS;
 				
-				CLEAR: next_state = countDone ? DONE_CLEAR : CLEAR;
+				CLEAR: next_state = ((clear_x == MAX_X_PIXELS) && (clear_y == MAX_Y_PIXELS)) ? DONE_CLEAR : CLEAR;
 				
 				DONE_CLEAR: next_state = DONE;
 				
@@ -110,11 +110,14 @@ module controlpath(
 			clearinitsignal = 1'b0;
 			loadenable = 1'b0;
 			commandaudioenable = 1'b0;
+			drawen = 1'b0;
+			
 			
 			case(current_state)
 			
 				LOAD_INPUTS: begin
 					loadenable = 1'b1;
+					drawen = 1'b1;
 					if (room0 == 1'b1) //when load is enabled, also send signal to multiplexer that inputs switch input into sw input register
 						selsw = 3'd0;
 					else if (room1 == 1'b1)
@@ -129,6 +132,7 @@ module controlpath(
 				
 				ROOM0: begin //When we have selected room 1, pass input function (L or D) and ON?OFF input to Room 0 registers
 					enable0 = 1'b1;
+					drawen = 1'b1;
 					if (keyboardin == 1 && audin == 0) begin//Enable signals for audio output
 						selonoff = 1'b0; //where 0 selects OFF
 						selfunct = 2'b00; //where 1 selects Light function
@@ -153,6 +157,7 @@ module controlpath(
 				
 				ROOM1: begin
 					enable1 = 1'b1;
+					drawen  = 1'b1;
 					if (keyboardin == 1 && audin == 0) begin//Enable signals for audio output
 						selonoff = 1'b0; //where 0 selects OFF
 						selfunct = 2'b00; //where 1 selects Light function
@@ -176,6 +181,7 @@ module controlpath(
 				
 				ROOM2: begin
 					enable2 = 1'b1;
+					drawen = 1'b1;
 					if (keyboardin == 1 && audin == 0) begin//Enable signals for audio output
 						selonoff = 1'b0; //where 0 selects OFF
 						selfunct = 2'b00; //where 1 selects Light function
@@ -199,6 +205,7 @@ module controlpath(
 				
 				ROOM3: begin
 					enable3 = 1'b1;
+					drawen = 1'b1;
 					if (keyboardin == 1 && audin == 0) begin//Enable signals for audio output
 						selonoff = 1'b0; //where 0 selects OFF
 						selfunct = 2'b00; //where 1 selects Light function
@@ -222,6 +229,7 @@ module controlpath(
 				
 				ROOM4: begin
 					enable4 = 1'b1;
+					drawen = 1'b1;
 					if (keyboardin == 1 && audin == 0) begin//Enable signals for audio output
 						selonoff = 1'b0; //where 0 selects OFF
 						selfunct = 2'b00; //where 1 selects Light function
@@ -241,10 +249,6 @@ module controlpath(
 						selonoff = 1'b1; 
 						selfunct = 2'b10; 
 					end
-				end
-				
-				ALLLOCKED: begin
-					selfunct = 2'b01;
 				end
 				
 				DONE: begin
@@ -275,4 +279,4 @@ module controlpath(
 		end
 				
 	
-endmodule		
+endmodule	
